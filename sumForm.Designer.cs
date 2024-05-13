c588aae3a18f711faeebb9b2ef3f2e61d0a72bc8
waldy <clocktoktok@gmail.com>
2024-05-13 08:03:16 +0700

    fiinish calculator for large number

--- sumForm.Designer.cs
+++ sumForm.Designer.cs
@@ -31,87 +31,107 @@
             this.txt_a = new System.Windows.Forms.Label();
             this.txt_b = new System.Windows.Forms.Label();
             this.btn_calc = new System.Windows.Forms.Button();
-            this.txt_c = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // txt_a
             // 
             this.txt_a.AutoSize = true;
-            this.txt_a.Location = new System.Drawing.Point(155, 38);
+            this.txt_a.Location = new System.Drawing.Point(116, 31);
+            this.txt_a.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txt_a.Name = "txt_a";
-            this.txt_a.Size = new System.Drawing.Size(50, 16);
+            this.txt_a.Size = new System.Drawing.Size(44, 13);
             this.txt_a.TabIndex = 0;
             this.txt_a.Text = "Input A:";
             // 
             // txt_b
             // 
             this.txt_b.AutoSize = true;
-            this.txt_b.Location = new System.Drawing.Point(155, 192);
+            this.txt_b.Location = new System.Drawing.Point(116, 156);
+            this.txt_b.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txt_b.Name = "txt_b";
-            this.txt_b.Size = new System.Drawing.Size(50, 16);
+            this.txt_b.Size = new System.Drawing.Size(44, 13);
             this.txt_b.TabIndex = 0;
             this.txt_b.Text = "Input B:";
             // 
             // btn_calc
             // 
-            this.btn_calc.Location = new System.Drawing.Point(40, 301);
+            this.btn_calc.Location = new System.Drawing.Point(30, 245);
+            this.btn_calc.Margin = new System.Windows.Forms.Padding(2);
             this.btn_calc.Name = "btn_calc";
-            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.Size = new System.Drawing.Size(56, 19);
             this.btn_calc.TabIndex = 2;
             this.btn_calc.Text = "sum";
             this.btn_calc.UseVisualStyleBackColor = true;
             this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
             // 
-            // txt_c
-            // 
-            this.txt_c.AutoSize = true;
-            this.txt_c.Location = new System.Drawing.Point(176, 304);
-            this.txt_c.Name = "txt_c";
-            this.txt_c.Size = new System.Drawing.Size(44, 16);
-            this.txt_c.TabIndex = 3;
-            this.txt_c.Text = "label1";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(777, 369);
+            this.button1.Location = new System.Drawing.Point(704, 333);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(94, 19);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "add test value";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(288, 12);
+            this.richTextBox1.Location = new System.Drawing.Point(216, 10);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(635, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(477, 79);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(288, 125);
+            this.richTextBox2.Location = new System.Drawing.Point(216, 102);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(635, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(477, 79);
             this.richTextBox2.TabIndex = 5;
             this.richTextBox2.Text = "";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 245);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "output:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(216, 213);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(477, 79);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
             // sumForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1079, 460);
+            this.ClientSize = new System.Drawing.Size(809, 374);
+            this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.txt_c);
             this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_b);
             this.Controls.Add(this.txt_a);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "sumForm";
             this.Text = "sumForm";
             this.Load += new System.EventHandler(this.sumForm_Load);
@@ -125,10 +145,11 @@
         private System.Windows.Forms.Label txt_a;
         private System.Windows.Forms.Label txt_b;
         private System.Windows.Forms.Button btn_calc;
-        private System.Windows.Forms.Label txt_c;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
     }
 }
 
--- sumForm.cs
+++ sumForm.cs
@@ -17,46 +17,137 @@ namespace TestInternship
         public sumForm()
         {
             InitializeComponent();
-            txt_c.Visible = false;
+        }
+
+        private List<string> convertStringToList(string str, int gap)
+        {
+            List<string> listOfString = new List<string>();
+
+            for (int i = str.Length; i > 0; i -= gap)
+            {
+                if (i - gap < 0)
+                {
+                    gap = i;
+                }
+
+                listOfString.Insert(0, str.Substring(i - gap, gap)); // Insert at the beginning to maintain order
+            }
+
+            return listOfString;
         }
 
         private void btn_calc_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("starting to calculate");
+            //get amount of digit in both textbox
             int aDigit = richTextBox1.Text.Length, bDigit = richTextBox2.Text.Length;
 
+            //bigint can handle 19 digits, make 18 as usage and the very first digits is to parse value
+            int amountOfNumberFromArrayToString = 18;
+
+            //checking the amount of array needed to store
             int amountArrayA = 0, amountArrayB = 0;
 
-            if (aDigit % 19 == 0)
+            if (aDigit % 18 == 0)
             {
-                amountArrayA = aDigit / 19;
+                amountArrayA = aDigit / 18;
             }
             else
             {
-                amountArrayA = (aDigit / 19) + 1;
+                amountArrayA = (aDigit / 18) + 1;
             }
 
-            if (bDigit % 19 == 0)
+            if (bDigit % 18 == 0)
             {
-                amountArrayB = (bDigit / 19);
+                amountArrayB = (bDigit / 18);
             }
             else
             {
-                amountArrayB = (bDigit / 19) + 1;
+                amountArrayB = (bDigit / 18) + 1;
             }
 
             BigInteger[] a = new BigInteger[amountArrayA];
             BigInteger[] b = new BigInteger[amountArrayB];
 
+            //Making everything in array to zero for format of calculating, prevent error might cause
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = BigInteger.Zero;
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = BigInteger.Zero;
+            }
+
+            //Make amount of arrayC = to the array of the biggest number
+            int amountArrayC = amountArrayA > amountArrayB ? amountArrayA : amountArrayB;
+
+            BigInteger[] c = new BigInteger[amountArrayC];
+
             StringBuilder stringA = new StringBuilder(richTextBox1.Text);
             StringBuilder stringB = new StringBuilder(richTextBox2.Text);
 
+            //convert each string to each clump of value in array with each value hold 18 digits of number
+            string[] arrayOfStringA = convertStringToList(stringA.ToString(), 18).ToArray();
+            string[] arrayOfStringB = convertStringToList(stringB.ToString(), 18).ToArray();
+
+            //looping through every single clump in array to turn every clumps of string to every clumps of big integer with 18 digits max and 1 digit to parse
             for (int s = 0; s < amountArrayA; s++)
             {
-                for (int i = 0; i < 19; i++)
-                {
-                    a[s] = BigInteger.Parse(stringA.ToString());
-                }
+                a[s] = BigInteger.Parse(arrayOfStringA[s]);
             }
+
+            for (int s = 0; s < amountArrayB; s++)
+            {
+                b[s] = BigInteger.Parse(arrayOfStringB[s]);
+            }
+
+            //create index of two array to loop through
+            int indexA = a.Length - 1;
+            int indexB = b.Length - 1;
+            for (int s = amountArrayC - 1; s >= 0; s--)
+            {
+                //handle if on of the array reach the end but the other still have element to loop through, treat it as zero
+                BigInteger valueA = indexA >= 0 ? a[indexA] : BigInteger.Zero;
+                BigInteger valueB = indexB >= 0 ? b[indexB] : BigInteger.Zero;
+                BigInteger tempSum = BigInteger.Zero;
+                tempSum = BigInteger.Add(valueA, valueB);
+
+                if (s > 0)
+                {
+                    if (tempSum.ToString().Length == 19)
+                    {
+                        //if the chunk of number is 19 digit, take 18 digit and the last digit will be 1 so convert it to 0 and bring that one to next chunk like basic math
+                        string temp = tempSum.ToString();
+
+                        temp = "0" + temp.Substring(1);
+                        tempSum = BigInteger.Parse(temp);
+                        c[s] = tempSum;
+                        c[s - 1] = BigInteger.Add(c[s - 1], BigInteger.One);
+                    }
+                    else
+                    {
+                        c[s] = tempSum;
+                    }
+                }
+                else
+                {
+                    c[s] = tempSum;
+                }
+                indexA--;
+                indexB--;
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (BigInteger d in c)
+            {
+                result.Append(d.ToString());
+            }
+
+            richTextBox3.Text = result.ToString();
+            Console.WriteLine("calculate done");
+            Console.WriteLine(result);
         }
 
         private void txtBox_a_KeyPress(object sender, KeyPressEventArgs e)
@@ -81,6 +172,16 @@ namespace TestInternship
 
         private void sumForm_Load(object sender, EventArgs e)
         {
+            if (richTextBox1.TextLength == 0)
+            {
+                richTextBox1.Text = "0";
+            }
+            if (richTextBox2.TextLength == 0)
+            {
+                richTextBox2.Text = "0";
+            }
+
+            richTextBox3.ReadOnly = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -103,6 +204,14 @@ namespace TestInternship
 
             richTextBox2.Text = numB.ToString();
             Console.WriteLine(richTextBox2.Text.Length + " B");
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsPunctuation(e.KeyChar) || char.IsSeparator(e.KeyChar) || char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
